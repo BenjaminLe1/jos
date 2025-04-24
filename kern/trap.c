9e18130ed2d6399102449b121112e8de185597ea
BenjaminLe1 <le166@purdue.edu>
2025-04-24 15:22:02 -0400

    LAB 5

--- kern/trap.c
+++ kern/trap.c
@@ -293,6 +293,10 @@ trap_dispatch(struct Trapframe *tf)
 	// Handle clock interrupts. Don't forget to acknowledge the
 	// interrupt using lapic_eoi() before calling the scheduler!
 	// LAB 4: Your code here.
+
+	// Handle keyboard and serial interrupts.
+	// LAB 5: Your code here.
+
 	
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
